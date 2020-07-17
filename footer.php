a1cd13c3a873718c4c90d4dc7d70340294c5bbbe
Ryuichi Umeno <good-days_with_big-smile@hotmail.co.jp>
2020-07-17 14:13:57 +0900

    footerのexhibitionリンク誤り修正

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
           <li class="c-fnav__menu01 p-fnav__list p-fnav__list--top"><a class="p-fnav__link" href="<?php echo home_url(); ?>">ホーム</a></li>
           <li class="c-fnav__menu03 p-fnav__list"><a class="p-fnav__link" href="<?php echo get_post_type_archive_link( 'works' ); ?>">製作実績一覧</a></li>
           <li class="c-fnav__menu04 p-fnav__list"><a class="p-fnav__link" href="<?php echo get_post_type_archive_link( 'blog' ); ?>">ブログ記事一覧</a></li>
-          <li class="c-fnav__menu06 p-fnav__list"><a class="p-fnav__link" href="<?php echo get_post_type_archive_link( '/exhibition' ); ?>">個人的な創作物一覧</a></li>
+          <li class="c-fnav__menu06 p-fnav__list"><a class="p-fnav__link" href="<?php echo get_post_type_archive_link( 'exhibition' ); ?>">個人的な創作物一覧</a></li>
           <li class="c-fnav__menu07 p-fnav__list"><a class="p-fnav__link" href="<?php echo home_url( '/about' ); ?>">DOUについて</a></li>
           <li class="c-fnav__menu08 p-fnav__list"><a class="p-fnav__link" href="<?php echo home_url( '/privacy' ); ?>">プライバシーポリシー</a></li>
           <li class="c-fnav__menu09 p-fnav__list"><a class="p-fnav__link" href="<?php echo home_url( '/contact' ); ?>">お問い合わせ</a></li>     
