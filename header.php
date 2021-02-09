c4267653b1037d75b9b45cee1ae9ff139c7b81c2
Ryuichi Umeno <good-days_with_big-smile@hotmail.co.jp>
2021-02-09 16:23:23 +0900

    ブログ記事のパーツについてデザインを調整

--- header.php
+++ header.php
@@ -1,18 +1,37 @@
 <!DOCTYPE html>
-<html lang="ja" >
-  <head>
-    <meta charset="<?php bloginfo ('charset') ; ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?php bloginfo('name'); ?></title>
-    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
-    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/style.css?ver=0.0.7" type="text/css">
-    <?php wp_head(); ?>
-    <link rel="SHORTCUT ICON" href="<?php bloginfo('template_url'); ?>/img/DOU-favicon.ico" />
-    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>  
-  </head>
-  
-  <body>
-    <div id="app">
+<html lang="ja">
 
+<head>
+  <!-- Google Tag Manager -->
+  <script>
+    (function(w, d, s, l, i) {
+      w[l] = w[l] || [];
+      w[l].push({
+        'gtm.start': new Date().getTime(),
+        event: 'gtm.js'
+      });
+      var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l != 'dataLayer' ? '&l=' + l : '';
+      j.async = true;
+      j.src =
+        'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+      f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-M425S65');
+  </script>
+  <!-- End Google Tag Manager -->
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title><?php bloginfo('name'); ?></title>
+  <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/style.css?ver=0.0.8" type="text/css">
+  <?php wp_head(); ?>
+  <link rel="SHORTCUT ICON" href="<?php bloginfo('template_url'); ?>/img/DOU-favicon.ico" />
+  <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+</head>
 
-
+<body>
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M425S65" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
+  <div id="app">
